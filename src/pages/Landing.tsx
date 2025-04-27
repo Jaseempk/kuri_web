ed85c85ce506026dfcced7185f9d600a045810ba
Jaseem <jasimpk011@gmail.com>
2025-04-27 16:37:42 +0530

    trigger_27-16:37:42

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -515,10 +515,6 @@ function App() {
           </motion.div>
         </motion.div>
 
-        {/* Decorative elements */}
-        <div className="absolute top-1/4 left-1/4 w-32 h-32 rounded-full border border-white/20 rotating-circle opacity-70"></div>
-        <div className="absolute bottom-1/4 right-1/4 w-48 h-48 rounded-full border border-white/10 rotating-circle-reverse opacity-50"></div>
-
         {/* Section transition blend effect */}
         <div className="absolute bottom-0 left-0 right-0 h-48 overflow-hidden">
           {/* Multiple wave layers with increasing opacity */}
