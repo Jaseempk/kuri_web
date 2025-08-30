2f704b596ee8febe064330dde6ed136c550bb1e3
Jaseem <jasimpk011@gmail.com>
2025-08-30 17:40:40 +0530

    feat:market detail mobile view udpated

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-41f5f610'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cjdu6ge8k5"
+    "revision": "0.5vb5petl7qo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
